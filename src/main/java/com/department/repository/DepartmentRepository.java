8076e41639d58ad60043fc9d1e1cc044529e45ac
persey86 <persey86@ukr.net>
2017-04-17 16:23:51 +0300

    It has been added repository-classes and their implementation

---
+++ src/main/java/com/department/repository/DepartmentRepository.java
@@ -0,0 +1,14 @@
+package com.department.repository;
+
+import com.department.exceptions.DepartmentRepositoryException;
+import com.department.models.Department;
+
+/**
+ * Created on 15.04.2017.
+ */
+public interface DepartmentRepository extends GeneralRepository <Department, Integer, DepartmentRepositoryException>{
+
+    Boolean isAnotherDepartmentHasThisName(Integer departmentId, String newDepartmentName) throws DepartmentRepositoryException;
+
+    Boolean isAnyDepartmentHasThisName(String newDepartmentName) throws DepartmentRepositoryException;
+}
